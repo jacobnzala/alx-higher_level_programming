128bfaf52910be4ea21317533e4918419fc61b6c
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-08-10 20:13:42 +0200

    Update 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,6 @@
---create a table in the database 
-CREATE TABLE first_table (
+-- creates a table called first_table in the current database in your MySQL server
+-- creates a table even if the table already exists
+CREATE TABLE IF NOT EXISTS first_table (
   id INT
   name VARCHAR(256)
 );
