285f713aae14aac0af83ef762932589b22300688
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-08-10 21:28:56 +0200

    Update 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
---Write a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
---inserting 2 element in the row with 2 values
-INSERT INTO first_table('id', 'name') VALUES (89, 'Best School');
+-- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
+-- inserts a new row in a table
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best School');
