c4aa7e54b90b14d070b0996f98d2d302af427e26
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-08-10 20:49:55 +0200

    Update 8-count_89.sql

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,1 +1,3 @@
- script that counts how many times the number is repeated
+ --script that counts how many times the number is repeated
+ --count how many times 89 is repeated
+ COUNT 89 FROM first_table;
