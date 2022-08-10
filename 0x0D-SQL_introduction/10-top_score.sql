f6066e9aded352137a049da00fee263fbf69f178
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-08-10 21:02:26 +0200

    Update 10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,1 +1,3 @@
-iWrite a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+-- displays all records of a table
+SELECT score, name FROM second_table ORDER BY score DESC;
